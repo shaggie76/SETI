d8dcd4815010e8a7d0b1095412da59c2792fec5c
Glen Miner <shaggie76@gmail.com>
2016-07-19 22:06:36 -0400

    Added Scripts for RAC/APP
    First digest host.gz into CSV of host id, RAC; second script bounces
    first 10K by RAC off of PHP to see if they run anonymous (lunatics) apps

---
+++ appHistogram.pl
@@ -0,0 +1,55 @@
+use strict;
+use warnings;
+
+my $fd;
+open($fd, "apps.csv") or die;
+
+my @bins;
+
+my $rank = 0;
+my $binSize = 10;
+
+my $bin = {binSize => $binSize, anonymous => 0, stock => 0};
+
+while(<$fd>)
+{
+    chomp;
+    my @col = split(/,/, $_);
+
+    if(scalar(@col) != 2)
+    {
+        print("Parse error: apps.csv $_\n");
+        next;
+    }
+
+    my $app = $col[1];
+
+    unless($app eq 'anonymous' || $app eq 'stock')
+    {
+        die;
+    }
+
+    ++$bin->{$app};
+
+    ++$rank;
+    if($rank == $binSize)
+    {
+        push(@bins, $bin);
+        $binSize = $binSize * 10;
+
+        $bin = { %$bin };
+        $bin->{binSize} = $binSize;
+    }
+}
+
+close($fd);
+
+print("Rank,Anonymous,Stock\n");
+foreach $bin (@bins)
+{
+    my $in = 1.0 / $bin->{binSize};
+    my $a = $bin->{anonymous} * $in;
+    my $s = $bin->{stock} * $in;
+
+    print($bin->{binSize} . ",$a,$s\n");
+}
---
+++ getApps.pl
@@ -0,0 +1,46 @@
+use strict;
+use warnings;
+
+my $MAX_HOSTS = 10000;
+my $BASE_URL = "http://setiathome.berkeley.edu/host_app_versions.php?hostid=";
+
+sub CheckHost($)
+{
+    my $hid = shift;
+
+    my $url = $BASE_URL . $hid;
+    my $app = "stock";
+
+    my $curl;
+    open($curl, "curl --silent $url 2>&1 |") or die;
+
+    while(<$curl>)
+    {
+        if(/anonymous platform/)
+        {
+            $app = "anonymous";
+            last;
+        }
+    }
+
+    close($curl);
+
+    print("$hid,$app\n");
+
+    sleep(1);
+}
+
+open(my $fd, "topHosts.csv") or die;
+
+while(<$fd>)
+{
+    if(/^([0-9]*),(.*)/)
+    {
+        CheckHost($1);
+
+        unless(--$MAX_HOSTS)
+        {
+            last;
+        }
+    }
+}
